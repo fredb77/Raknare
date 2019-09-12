22641f30678761e66f1d3ccc9e3e1cfc57c201eb
Fredrik Byström <fredbstrom@gmail.com>
2019-09-12 21:54:36 +0200

    ändrade i script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var svarLista = document.getElementById("svar-lista");
 
 // funktion som skriver ut en lista på talen man räknat
 function sparaLista(array){
-	
+
     for(i = antal; i < array.length; i++){
         var li1 = document.createElement("li");
         var txt = document.createTextNode(array[i]);
@@ -20,20 +20,20 @@ function rakna() {
 
 	var tal1 = document.getElementById("tal-1").value;
 	var tal2 = document.getElementById("tal-2").value;
-	var vad = document.getElementById("vad").selectedIndex;				// hämtar värdet från select taggen
+	var vad = document.getElementById("vad").selectedIndex;			// hämtar värdet från select taggen
 	var rakna = document.getElementsByTagName("option")[vad].value; 
 	var sum = 0, result = 0;
 
 	var slista = document.getElementById("svar-lista");
 	slista.style.display = "block";
 
-	if (isNaN(tal1) || isNaN(tal2)) {									// kollar om det var ett nummer som skrevs in 
+	if (isNaN(tal1) || isNaN(tal2)) {								// kollar om det var ett nummer som skrevs in 
 		slista.innerHTML = "Skriv ett nummer istället !!!";
 	}else {
 
-		switch (rakna)													// här använder den värdet som kommer från
-		{																// select, och beroende på vad som valdes
-			case "plus":												// så kommer någon av casearna köras
+		switch (rakna)												// här använder den värdet som kommer från
+		{															// select, och beroende på vad som valdes
+			case "plus":											// så kommer någon av casearna köras
 				sum = Number(tal1) + Number(tal2);
 				result = tal1 + " + " + tal2 + " = " + sum;
 				svar2.push(result);
@@ -64,6 +64,10 @@ function rakna() {
 				sparaLista(svar2);
 				antal++;
 				break;
-		}															// eftersom det inte kan bli något annat än de case som finns 
-	}																// så har jag ingen default		
+		}
+	}																
 }
+/*
+eftersom det inte kan bli något annat än de case som finns
+så har jag ingen default	
+*/
